8f3bce91eb2ef8deac2b529cca4405e954f5c3e2
jiansoung <Jian.Soung@outlook.com>
2018-10-23 21:47:29 +0800

    Update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,11 @@ import tempfile
 
 import pytest
 
-from app import app
+from app import create_app
 
 
 @pytest.fixture
 def client():
+    app = create_app()
     client = app.test_client()
     yield client
