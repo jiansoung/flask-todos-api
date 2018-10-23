6674c265237a16b6793ab6ffb862930ca48da67a
jiansoung <Jian.Soung@outlook.com>
2018-10-23 10:02:22 +0800

    Add Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import models
 from app import controllers
